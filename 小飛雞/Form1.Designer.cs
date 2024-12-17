a7e15d527824e33d81c4976b07057884b2ecba5a
Anna <anna0983266723@gmail.com>
2024-12-17 19:25:14 +0800

    加入專案檔案。

---
+++ 小飛雞/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace 小飛雞
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(125, 401);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 24);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.ErrorImage = null;
+            this.pictureBox2.Image = global::小飛雞.Properties.Resources.Flappy_Bird_up;
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(873, 109);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(59, 560);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 2500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::小飛雞.Properties.Resources.Day;
+            this.ClientSize = new System.Drawing.Size(944, 681);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ 小飛雞/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 小飛雞
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // 設定 Form 啟動時的位置
+            this.StartPosition = FormStartPosition.CenterScreen; // 視窗居中顯示
+        }
+        Random newpic_top = new Random();  //柱子的位置移動亂數
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer2.Start();   //柱子開始的timer
+            pictureBox1.Location = new Point(125, 200);   //小雞定位
+            timer1.Start();   //小雞的位置開始下降timer
+            pictureBox2.Visible = false;   //柱子的範本，因為有出錯所以timer停止、顯示隱藏
+
+            // 設置窗體能夠捕捉鍵盤事件 
+            this.KeyPreview = true;
+            // 綁定 KeyDown 事件 
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Top += 15;    //小雞下降
+            if (pictureBox1.Top >= 700|| pictureBox1.Top <= 0)    //如果碰倒邊界停止遊戲
+            {
+                timer1.Stop();
+                MessageBox.Show("game over");   //顯示遊戲結束
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBox1.Top -= 40;   //點擊後小雞會飛起
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            //柱子定位參考
+            //left:900
+            //x=1 高:-350~-150
+            //x=0 低:450~250
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //設定柱子出來時機
+            timer2.Interval = (newpic_top.Next(2500, 3000));  
+            // 建立 PictureBox 分身
+            PictureBox newPictureBox = new PictureBox();  
+            newPictureBox.Size = pictureBox2.Size;    // 以 pictureBox2 為模板複製
+            int x = newpic_top.Next(0, 2);   //利用亂數來決定上還是下的柱子
+            switch (x)
+            {
+                case 0:
+                    newPictureBox.Image = Properties.Resources.Flappy_Bird_down; //newPictureBox開啟圖片向上的
+                    newPictureBox.Location = new Point(918, newpic_top.Next(370, 530));  // 設定位置
+                    break;
+
+                case 1:
+                    newPictureBox.Image = Properties.Resources.Flappy_Bird_up;   //newPictureBox開啟圖片向下的
+                    newPictureBox.Location = new Point(918, newpic_top.Next(-400, -230));  // 設定位置
+                    break;
+            }
+            newPictureBox.Visible = true;  // 顯示分身
+            this.Controls.Add(newPictureBox);  // 將分身加入到表單的控制項集合中
+
+            // 使用 Timer 來移動 newPictureBox
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();   
+            //因為newPictureBox沒法在建立的timer3物件中顯示，只能在此建立一個timer來用
+            timer.Interval = 1; // 設定移動的時間間隔 (20 毫秒)
+            timer.Tick += (obj, y) =>
+            {
+                newPictureBox.Left -= 15;   //newPictureBox移動
+
+                Rectangle rect1 = pictureBox1.Bounds;   //設定picturebox的界線
+                Rectangle rect2 = newPictureBox.Bounds;
+
+                if (newPictureBox.Left <= -30)   //如果newPictureBox碰到底部後消失
+                {
+                    timer.Stop();
+                    this.Controls.Remove(newPictureBox);
+                    newPictureBox.Dispose();
+                    timer.Dispose();
+                }
+                else if (rect1.IntersectsWith(rect2))   //如果newPictureBox碰到小雞後消失
+                {
+                    timer.Stop();   
+                    this.Controls.Remove(newPictureBox);   //刪除分身
+                    newPictureBox.Dispose();    //解放資訊
+                    timer.Dispose();     
+                    timer1.Stop();    //停止動作
+                    timer2.Stop();
+                    this.Controls.Clear();   //將柱子建立的分身存入之類的
+                    MessageBox.Show("game over");   //顯示遊戲結束
+                }
+            };
+
+            // 啟動 Timer
+            timer.Start();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // 檢查是否按下了空白鍵 
+            if (e.KeyCode == Keys.Space)  //其他按鍵：Keys.W
+            {
+                pictureBox1.Top -= 40;   //點擊後小雞會飛起
+            }
+
+        }
+    }
+}
